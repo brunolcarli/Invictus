7680354c8a9613c0f238a5717874dea4b1b0c03e
Bruno L. Carli <brunolcarli@gmail.com>
2022-09-16 18:44:09 -0300

    CHANGE hourly highscore crawl

--- ogame/crawlers.py
+++ ogame/crawlers.py
@@ -18,9 +18,17 @@ class OgameStatsCrawler:
         return data
 
     @staticmethod
-    def update_player_data(data):
+    def get_highscore_data():
+        data = ogame_stats.HighScoreQuestions(
+            OgameStatsCrawler.SERVER_ID,
+            OgameStatsCrawler.COMMUNITY
+        )
+        return data
+
+    @staticmethod
+    def update_player_data(data, player_id, highscores):
         player, _ = Player.objects.get_or_create(
-            player_id=int(data['id']),
+            player_id=int(player_id),
             server_id=data['serverId']
         )
 
@@ -29,63 +37,75 @@ class OgameStatsCrawler:
         player.planets = CompressedDict(data['planets']).bit_string
         player.save()
 
+        dt_reference = datetime.utcnow()
         score, created = Score.objects.get_or_create(
             player=player,
-            timestamp=int(data['timestamp'])    
+            timestamp=dt_reference.timestamp()
         )
 
         if not created:
             return
 
         print(f'Updating score for player: {player.player_id}:{player.name}')
-        scores = {}
-        switch = {
-            '0': 'total',
-            '1': 'economy',
-            '2': 'research',
-            '3': 'military',
-            '4': 'military_built',
-            '5': 'military_destroyed',
-            '6': 'military_lost',
-            '7': 'honor'
-        }
-        for position in data['positions']['position']:
-            if int(position['type']) > 7:
-                continue
+        player_id = str(player_id)
+        try:
+            total = highscores.total[['position', 'score']].loc[highscores.total.id == player_id].values[0]
+            economy = highscores.economy[['position', 'score']].loc[highscores.economy.id == player_id].values[0]
+            research = highscores.research[['position', 'score']].loc[highscores.research.id == player_id].values[0]
+            military = highscores.military[['position', 'score', 'ships']].loc[highscores.military.id == player_id].values[0]
+            military_built = highscores.military_built[['position', 'score']].loc[highscores.military_built.id == player_id].values[0]
+            military_destroyed = highscores.military_destroyed[['position', 'score']].loc[highscores.military_destroyed.id == player_id].values[0]
+            military_lost = highscores.military_lost[['position', 'score']].loc[highscores.military_lost.id == player_id].values[0]
+            honor = highscores.honor[['position', 'score']].loc[highscores.honor.id == player_id].values[0]
+        except IndexError:
+            print(f'Failed retrieving {player.name} score')
+            return
 
-            score_type = position['type']
-            if score_type == '3':
-                scores[switch[score_type]] = CompressedDict({
-                    'score': float(position['score']),
-                    'rank': int(position['#text']),
-                    'ships': int(position['ships'])
-                }).bit_string
-
-            else:
-                scores[switch[score_type]] = CompressedDict({
-                    'score': float(position['score']),
-                    'rank': int(position['#text'])
-                }).bit_string
-
-        score.total = scores['total']
-        score.economy = scores['economy']
-        score.research = scores['research']
-        score.military = scores['military']
-        score.military_built = scores['military_built']
-        score.military_destroyed = scores['military_destroyed']
-        score.military_lost = scores['military_lost']
-        score.honor = scores['honor']
-        score.datetime = datetime.fromtimestamp(score.timestamp)
+        score.total = CompressedDict({
+            'score': float(total[0]),
+            'rank': int(total[1])}
+        ).bit_string
+        score.economy = CompressedDict({
+            'score': float(economy[0]),
+            'rank': int(economy[1])}
+        ).bit_string
+        score.research = CompressedDict({
+            'score': float(research[0]),
+            'rank': int(research[1])}
+        ).bit_string
+        score.military = CompressedDict({
+            'score': float(military[0]),
+            'rank': int(military[1]),
+            'ships': int(military[2])
+        }).bit_string
+        score.military_built = CompressedDict({
+            'score': float(military_built[0]),
+            'rank': int(military_built[1])}
+        ).bit_string
+        score.military_destroyed = CompressedDict({
+            'score': float(military_destroyed[0]),
+            'rank': int(military_destroyed[1])}
+        ).bit_string
+        score.military_lost = CompressedDict({
+            'score': float(military_lost[0]),
+            'rank': int(military_lost[1])}
+        ).bit_string
+        score.honor = CompressedDict({
+            'score': float(honor[0]),
+            'rank': int(honor[1])}
+        ).bit_string
+        score.datetime = dt_reference
         score.save()
 
     @staticmethod
     def crawl():
         while True:
             universe = OgameStatsCrawler.get_universe_data()
-            for player in universe.players.name.values:
+            highscores = OgameStatsCrawler.get_highscore_data()
+            for player_id, player_name in universe.players[['id', 'name']].values:
                 try:
-                    data = universe.get_player_data(player)
-                    OgameStatsCrawler.update_player_data(data['playerData'])
+                    data = universe.get_player_data(player_name)
+                    OgameStatsCrawler.update_player_data(data['playerData'], player_id, highscores)
                 except:
                     continue
-            sleep(3600*12)
+            sleep(3600*2)
