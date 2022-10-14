8063c998c8977c2fdcfcf7143a3c2a41fe2f4823
Bruno L. Carli <brunolcarli@gmail.com>
2022-10-14 03:11:04 -0300

    ADD schema mutations and login method

--- invictus/schema.py
+++ invictus/schema.py
@@ -1,10 +1,16 @@
 import graphene
 import ogame.schema
-
+import graphql_jwt
 
 
 class Query(ogame.schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(graphene.ObjectType, ogame.schema.Mutation):
+    log_in = graphql_jwt.ObtainJSONWebToken.Field()
+    validate_user_token = graphql_jwt.Verify.Field()
+    refresh_user_token = graphql_jwt.Refresh.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
